e0cf643b8c016394e4069c38be8ef6da5f1a5967
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 18:59:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,11 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
+    
   end
 
   def reverse_transfer
