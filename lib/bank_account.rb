d4869dc25ab0f242048876522cb9ae213b2937bc
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 18:50:08 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,22 @@
 class BankAccount
   attr_accessor :balance, :status
   att_reader :name
-  
+
   def initialize (name)
     @name = name
     @balance = 1000
     @status = "open"
   end
+
+  def deposit (amount)
+    @balance += amount
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false    
+    end
 end
