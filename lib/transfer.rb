b90dadd466e46d03822389a87b5e0411207f3c4a
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 19:03:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid?
+    if self.valid? && self.status != "complete"
       sender.balance -= @amount
       receiver.balance += @amount
       self.status = "complete"
